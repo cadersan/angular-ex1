cd48f11177bdf40750855402354382ca378fc43f
peter <peter_m12345@hotmail.com>
2025-03-07 19:35:47 +0200

    my firt commit

--- src/app/components/home-screen/home-screen.component.ts
+++ src/app/components/home-screen/home-screen.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home-screen',
@@ -6,18 +7,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-screen.component.css']
 })
 export class HomeScreenComponent {
-  items = [
+
+    number=0;
+    names=[""]
+    hidden=true
+    WordHidde=false;
+   
+    items = [
     {
       img: "assets/icons/mugitem.png",
+      title:"mug",
       price: "30$"
     },
     {
       img: "assets/icons/shoesitem.png",
+      title:"shoes",
       price: "90$"
     },
     {
       img: "assets/icons/caseitem.png",
+      title:"case",
       price: "12$"
     }
   ]
+  SelectImg(name:string){
+    this.number+=1;
+  
+    this.names.push(name);
+
+  }
+  buttonExpand(){
+    this.hidden=false
+    this.WordHidde=true;
+  }
+
+  buttonHidde(){
+    this.hidden=true
+    this.WordHidde=false
+  }
+ 
 }
--- src/app/components/side-bar/side-bar.component.ts
+++ src/app/components/side-bar/side-bar.component.ts
@@ -6,6 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./side-bar.component.css']
 })
 export class SideBarComponent {
-  activeItemIndex = 0;
+  activeItemIndex=0;
+  name1="";
+  name2="";
+  name3="";
+  
   items=["My Cart", "Products", "About", "Contact us"]
+
+  button1(i:number){
+    
+    this.activeItemIndex=i
+  }
 }
